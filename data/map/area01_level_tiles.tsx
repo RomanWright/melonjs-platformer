a350faca0342dc674acbff2217e71c6ce7f52a77
Roman Wright <rwright@berkeley.edu>
2018-05-16 18:58:18 -0700

    map

---
+++ data/map/area01_level_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="area01_level_tiles.png" tilewidth="32" tileheight="32" tilecount="140" columns="20">
+ <image source="github/melonjs-platformer/data/img/map/area01_level_tiles.png" width="640" height="241"/>
+</tileset>
